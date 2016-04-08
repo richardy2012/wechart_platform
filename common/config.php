564d2f20e20ca12de6a69637bdc4c55bcd40f9b8
sweng <shiqiang-tommy.weng@nokia.com>
2016-04-08 06:58:56 +0200

    Modify mysql user and passwd

--- common/config.php
+++ common/config.php
@@ -6,8 +6,8 @@ $CONFIG['db'] = array(
     'host'           =>      'localhost',
 	'port'           =>      '3306',
     'user'           =>      'root',
-    'password'       =>      'root123',
-    'database'       =>      'jeeweixin',
+    'password'       =>      '',
+    'database'       =>      'phpweixin',
     'charset'        =>      'utf8',
     'pconnect'       =>      false,             //数据库连接标识; pconn 为长久链接，默认为即时链接
 );
